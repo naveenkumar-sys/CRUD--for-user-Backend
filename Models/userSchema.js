8740a20137acc26de99a303fa7e2eed1f0804a2b
naveenkumar-sys <naveenrnk69@gmail.com>
2025-12-23 10:49:04 +0530

    Model completed

---
+++ Models/userSchema.js
@@ -0,0 +1,34 @@
+import { TokenExpiredError } from "jsonwebtoken";
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  UserAge: {
+    type: Number,
+    required: true,
+  },
+  userDOB: {
+    type: String,
+    required: true,
+  },
+  userProfile: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+});
+
+const user = mongoose.model("user", userSchema);
+
+export default user;
